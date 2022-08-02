4e6696425af438865fe6a59ca220d4c0d1fd34b7
BABAR1122786 <93757988+BABAR1122786@users.noreply.github.com>
2022-08-02 18:49:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front End Developer", "Back End Developer", "Full Stck Developer", "Blogger", "Freelancer"],
+        strings: ["Front End Developer", "Back End Developer", "Full Stack Developer", "Blogger", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front End Developer", "Back End Developer", "Full Stck Developer", "Blogger", "Freelancer"],
+        strings: ["Front End Developer", "Back End Developer", "Full Stack Developer", "Blogger", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
